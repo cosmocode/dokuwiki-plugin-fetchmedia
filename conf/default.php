90b50c8d9bdaea68fe2318bd2e62f0a673b57444
Michael Große <grosse@cosmocode.de>
2017-07-20 12:06:24 +0200

    Initial commit

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the fetchmedia plugin
+ *
+ * @author Michael Große <dokuwiki@cosmocode.de>
+ */
+
+//$conf['fixme']    = 'FIXME';
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the fetchmedia plugin
+ *
+ * @author Michael Große <dokuwiki@cosmocode.de>
+ */
+
+
+//$meta['fixme'] = array('string');
+
