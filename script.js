718b2b7f88903a1020d59df311395511e0509e49
Michael Große <grosse@cosmocode.de>
2017-07-26 10:58:55 +0200

    Remove jQuery from main js codebase
    The is a prerequisite to compiling that code with babel.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 jQuery(() => {
     'use strict';
 
-    const $form = jQuery('#fetchmedia_form');
+    const form = document.getElementById('fetchmedia_form');
 
 
     function* flattenLinks(data) {
@@ -18,27 +18,22 @@ jQuery(() => {
         }
 
         const [page, link] = value;
+        const options = {
+            method: 'POST',
+            headers: new Headers({ 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8' }),
+            body: Object.entries({
+                call: 'plugin_fetchmedia_downloadExternalFile',
+                page: page,
+                link: link,
+            }).map(([k, v]) => {
+                return k + '=' + encodeURIComponent(v);
+            }).join('&'),
+            credentials: 'include',
+        };
 
-        fetch(
-            DOKU_BASE + 'lib/exe/ajax.php',
-            {
-                method: 'POST',
-                headers: new Headers({ 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8' }),
-                body: Object.entries({
-                    call: 'plugin_fetchmedia_downloadExternalFile',
-                    page: page,
-                    link: link,
-                }).map(([k, v]) => {
-                    return k + '=' + encodeURIComponent(v);
-                }).join('&'),
-                credentials: 'include',
-            },
-        )
+        fetch(DOKU_BASE + 'lib/exe/ajax.php', options)
             .then((res) => {
-                console.log('success');
-                console.log(res);
                 const selector = `li[data-id="${btoa(page + link)}"]`;
-                console.log(selector);
                 const li = document.querySelector(selector);
                 const STATUS_OK = 200;
                 if (res.status === STATUS_OK) {
@@ -56,30 +51,40 @@ jQuery(() => {
             });
     }
 
-    $form.submit(
+    form.addEventListener('submit',
         function (event) {
             event.preventDefault();
-            const options = {
+            const body = {
                 call: 'plugin_fetchmedia_getExternalMediaLinks',
-                namespace: $form.find('input[name="namespace"]').val(),
-                type: $form.find('input[name="mediatypes"]:checked').val(),
-                sectok: $form.find('input[name="sectok"]').val(),
+                namespace: form.querySelector('input[name="namespace"]').value,
+                type: form.querySelector('input[name="mediatypes"]:checked').value,
+                sectok: form.querySelector('input[name="sectok"]').value,
             };
-            jQuery.get(DOKU_BASE + 'lib/exe/ajax.php', options).done(function displayPagesToDownload(data) {
-                const tableHead = '<table class="inline"><thead><tr><th>Page 📄</th><th>Links 🔗</th></tr></thead>';
-                const tableRows = Object.entries(data).map(([page, mediaLinks]) =>
-                    `<tr>
+            const query = Object.entries(body).map(([k, v]) => {
+                return k + '=' + encodeURIComponent(v);
+            }).join('&');
+            const options = {
+                method: 'GET',
+                headers: new Headers({ 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8' }),
+                credentials: 'include',
+            };
+            fetch(`${DOKU_BASE}lib/exe/ajax.php?${query}`, options)
+                .then(response => response.json())
+                .then(function displayPagesToDownload(data) {
+                    const tableHead = '<table class="inline"><thead><tr><th>Page 📄</th><th>Links 🔗</th></tr></thead>';
+                    const tableRows = Object.entries(data).map(([page, mediaLinks]) =>
+                        `<tr>
                         <td><span class="wikipage">${page}</span></td>
                         <td><ul>${mediaLinks.map(url => `<li data-id="${btoa(page + url)}">${url}</li>`).join('')}</ul></td>
-                    </tr>`
-                );
-                // todo handle case that there are no external links
-                const table = tableHead + tableRows.join('') + '</table>';
-                jQuery('#fetchmedia_results').html(table);
+                    </tr>`,
+                    );
+                    // todo handle case that there are no external links
+                    const table = tableHead + tableRows.join('') + '</table>';
+                    document.getElementById('fetchmedia_results').innerHTML = table;
 
-                const linkGen = flattenLinks(data);
-                requestDownloadExternalFile(linkGen);
-            });
+                    const linkGen = flattenLinks(data);
+                    requestDownloadExternalFile(linkGen);
+                });
         },
     );
 });
