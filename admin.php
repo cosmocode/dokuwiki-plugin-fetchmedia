9fd27773d45caad8afe43fb2bb130a3b1eb16e78
Michael Große <grosse@cosmocode.de>
2017-07-28 18:16:28 +0200

    feat(form): add basic attributes (required, etc.)

--- admin.php
+++ admin.php
@@ -22,15 +22,31 @@ class admin_plugin_fetchmedia extends DokuWiki_Admin_Plugin {
     public function html() {
         $doc = '<h1>'.$this->getLang('menu').'</h1>';
 
-        $form = new \dokuwiki\Form\Form(['id' => 'fetchmedia_form']);
-        $form->addTextInput('namespace', 'Namespace to download');
-        $form->addRadioButton('mediatypes', 'All types of Media')->val('all');
-        $form->addRadioButton('mediatypes', 'Only Windows-File-Shares')->val('windows-shares');
-        $form->addRadioButton('mediatypes', 'Only common media files')->val('common');
-        $form->addButton('submit', 'Download');
 
+        $form = new \dokuwiki\Form\Form(['id' => 'fetchmedia_form']);
+        $form->addFieldsetOpen();
+        $form->addTextInput('namespace', 'Namespace to download')->attrs([
+            'inputmode' => 'verbatim',
+            'pattern' => '[a-z0-9_:/;\.]+',
+            'placeholder' => 'name:space',
+            'title' => 'Valid namespaces can consist of lowercase letters, numbers and the _ and : characters',
+            'required' => 'required',
+            'autofocus' => 'autofocus,'
+        ]);
+        $form->addHTML('<br />');
+        $form->addRadioButton('mediatypes', 'All types of Media')->val('all')->attr('required', 'required');
+        $form->addRadioButton('mediatypes', 'Only Windows-File-Shares')->val('windows-shares')->attrs([
+            'required' => 'required',
+            'checked' => 'checked',
+        ]);
+        $form->addRadioButton('mediatypes', 'Only common media files')->val('common')->attr('required', 'required');
+        $form->addButton('submit', 'Download');
+        $form->addFieldsetClose();
+
+        $doc .= '<div id="plugin_fetchmedia_page">';
         $doc .= $form->toHTML();
         $doc .= '<div id="fetchmedia_results"></div>';
+        $doc .= '</div>';
         echo $doc;
     }
 }
