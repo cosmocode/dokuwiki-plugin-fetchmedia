af9200f31a70160f5fd279799104c55df7f650e4
Michael Große <grosse@cosmocode.de>
2017-08-02 13:30:33 +0200

    Added German translation

---
+++ lang/de/lang.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * German language file for fetchmedia plugin
+ *
+ * @author Michael Große <dokuwiki@cosmocode.de>
+ */
+
+// menu entry for admin plugins
+$lang['menu'] = 'Externe Mediendateien herunterladen';
+
+
+$lang['label: namespace input'] = 'Namensraum';
+$lang['label: all media'] = 'Beide Arten von externen Medienlinks';
+$lang['label: windows shares'] = 'Nur Windows-File-Shares';
+$lang['label: common media only'] = 'Nur gewöhnlich eingebundene Mediendateien';
+$lang['label: button download'] = 'Jetzt herunterladen';
+
+$lang['title: namespace input hint'] = 'Ein Namensraum kann aus a-z, Zahlen und _ und : bestehen';
+
+$lang['error: windows share missing'] = 'Windows-File-Share existiert nicht';
+$lang['error: failed to open stream'] = 'Konnte Stream zum Dateidownload nicht öffen';
+$lang['error: failed to download file'] = 'Herunterladen der Datei ist fehlgeschlagen';
+
+$lang['js']['table-heading: page'] = 'Seite 📄';
+$lang['js']['table-heading: links'] = 'Links 🔗';
+$lang['js']['error: no links found'] = 'Im angegebenen Namensraum wurden keine externen Links gefunden.';
+
+
+//Setup VIM: ex: et ts=4 :
